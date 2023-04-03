78a9eb5eebdfada202dda716e80d2b114fa518f3
Muhammed <faheem@metrictreelabs.com>
2023-04-03 16:52:44 +0530

    Initial Commit

---
+++ clock/clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockApp.swift
+//  clock
+//
+//  Created by FAHEEM on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
